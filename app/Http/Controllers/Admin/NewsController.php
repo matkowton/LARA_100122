2884a8c41c2295c613da78c279f8d031742c56e3
Poluyanov Ihor <matkowton@gmail.com>
2022-01-13 21:51:33 +0300

    lesson_2

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function index()
+    {
+        dd('jdkdfjkldjkljdkl');
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Admin\NewsController as AdminNewsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,4 +22,59 @@ Route::get('/', function () {
 Route::get('/test', function () {
     return '<b>hello, world!</b>';
 });
+
+Route::get('/news', [\App\Http\Controllers\NewsController::class, 'index'])
+    ->name("news::catalog");
+
+Route::get('/news/card/{id}', [\App\Http\Controllers\NewsController::class, 'card'])
+    ->where('id', '[0-9]+')
+    ->name("news::card");
+
+Route::resource('admin/category', \App\Http\Controllers\Admin\CategoryController::class);
+
+
+Route::get('/admin/news/', [\App\Http\Controllers\Admin\NewsController::class, 'index'])->name("admin::news::index");
+Route::get('/admin/news/create', [\App\Http\Controllers\Admin\NewsController::class, 'create'])->name("admin::news::create");
+Route::get('/admin/news/update', [\App\Http\Controllers\Admin\NewsController::class, 'update'])->name("admin::news::update");
+Route::get('/admin/news/delete', [\App\Http\Controllers\Admin\NewsController::class, 'delete'])->name("admin::news::delete");
+
+/**
+ * Админка новостей
+ */
+
+Route::group([
+    'prefix' => '/admin/news',
+    'as' => 'admin::news::'
+], function () {
+    Route::get('', [AdminNewsController::class, 'index'])
+        ->name("index");
+
+    Route::get('create', [AdminNewsController::class, 'create'])
+        ->name("create");
+
+    Route::get('update', [AdminNewsController::class, 'update'])
+        ->name("update");
+
+    Route::get('delete', [AdminNewsController::class, 'delete'])
+        ->name("delete");
+});
+
+
+
+
+/*
+Route::match(['get', 'post'], '/test', function (){return '<b>hello, world!</b>'; });
+Route::any( '/test', function (){return '<b>hello, world!</b>'; });
+*/
+
+/*
+Route::get('', function (){});
+Route::post('', function (){});
+Route::put('', function (){});
+Route::patch('', function (){});
+Route::delete('', function (){});
+Route::options('', function (){});
+*/
+
+
 
